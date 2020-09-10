8bfbf16e4c5cbb0f91be13eeff87773d17ddd8ca
hieupoo16022001 <54589137+hieupoo16022001@users.noreply.github.com>
2020-09-10 13:52:12 +0700

    lab7

---
+++ LAB_SEVEN7/LAB_SEVEN7/Book.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_SEVEN7
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public String Title { get; set; }
+        public String Author { get; set; }
+        public String CoverImage { get; set; }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+}
